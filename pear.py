db585e05369086a020d8f90cfc9946e224a39965
swissChili <sasha@fastmail.com>
2018-09-01 17:01:37 -0700

    Using sockets now, kinda works

--- pear.py
+++ pear.py
@@ -1,18 +1,62 @@
-import pear_server as ps
+import inquirer
+import pear_server as pear
 import requests
+import threading
 import ipaddress
-from bottle import request, post, get, run
+import sys
+import os
+import socket
 
 def prompt():
     return input("> ")
-
+"""
 ip = requests.get("http://bot.whatismyipaddress.com")
 ip = ip.text
 print("Your IP addr",ip)
+"""
 
-@post("/pear")
-def on_msg():
-    postdata = request.body.read()
-    print(postdata) 
+serve = [
+    inquirer.List('method',
+                   message="Create server or connect to one?",
+                   choices=['Create', 'Connect'])
+]
+answers = inquirer.prompt(serve)
+if answers['method'] == 'Connect':
+    # connect to server
+    print("connecting")
+else:
+    serve_info = [
+        inquirer.Text('host',
+                       message="Choose a host to serve on",
+                       default="ipv6"),
+        inquirer.Text('port',
+                       message="Choose a port to serve on, defaults to 4792",
+                       default="4792")
+    ]
+    host_info = inquirer.prompt(serve_info)
+    if host_info['host'] == "ipv6":
+        host_info['host'] = ''
+    ip = host_info['host']
+    port = int(host_info['port'])
 
-run(host=ip, port=2307)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ip, port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            def prompter():
+                data = prompt()
+                conn.sendall(bytes(data + "\r\n", 'utf8'))
+            def read():
+                while True:
+                    prompter()
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(data.decode('utf8'))
+            responder = threading.Thread(target = read())
+            prompter = threading.Thread(target = prompter())
+            prompter.start()
+            responder.start()
+            
--- pear_server.py
+++ pear_server.py
@@ -19,4 +19,3 @@ def encrypt(pub, msg):
 
 def decrypt(key, msg):
     key.decrypt(ast.literal.eval(str(msg)))
-
