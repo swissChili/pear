8f671d728aba45dde85c4f08d68fc466c6dbdf73
swissChili <sasha@fastmail.com>
2018-09-04 15:47:16 -0700

    Fixed validation issue, was missing paren

--- pear/src/pear/core.clj
+++ pear/src/pear/core.clj
@@ -7,6 +7,8 @@
 (def genesis-block {
   :nonce 0
   :hash "GENESIS"
+  :sender "God"
+  :message "Let there be light!"
 })
 (defn append-block
   "appends a block to the chain"
@@ -30,7 +32,6 @@
   "this validates the chain by checking the last block and
   recursively checking all before it"
   [chain]
-  (println (count @chain))
   (if (> (count @chain) 1) ;; if this is not the first block
                            ;; check all previous blocks
     (let [new-chain (atom (drop-last @chain))]
@@ -42,23 +43,18 @@
             (do
             (println "IF evaluated to true" (:hash last-block))
             (validate new-chain))
-            (println "false!"))))))
+            false))))
   (if (= genesis-block
         (first @chain))
     (do
-      (println "Checkin genesis blokk")
-      true) 
-    false)) ;; if it is first, return true
+      true)
+    false))) ;; if it is first, return true
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (append-block blockchain genesis-block)
-  (append-block blockchain {
-    :nonce 234123123
-    :hash "this is not a real blocc"
-  })
   ;;(println (json/write-str
     ;;@(add-block blockchain "ried" "ur mum big bad dumb")))
   ;;(println @blockchain)
-  (println "Blokk chain valid? " (validate blockchain)))
+  (println "Validating blockchain..." (validate blockchain)))
